2cf048475114a1dd2af14ab38f202b23841ea635
Dennis Thinh Tan Nguyen <dennisnguyen3000@yahoo.dk>
2023-08-20 09:09:43 +0200

    add Reasons

---
+++ csharp/Logic/DomainObjects/Constants/GeneralReasons.cs
@@ -0,0 +1,6 @@
+namespace Todo.Logic.DomainObjects.Constants;
+
+public static class GeneralReasons
+{
+    public const string InternalError = "INTERNAL_ERROR";
+}
---
+++ csharp/Logic/DomainObjects/Constants/TodoReasons.cs
@@ -0,0 +1,7 @@
+namespace Todo.Logic.DomainObjects.Constants;
+
+public static class TodoReasons
+{
+    public const string TodoNotFound = "TODO_NOT_FOUND";
+    public const string TodoAlreadyCreated = "TODO_ALREADY_CREATED";
+}

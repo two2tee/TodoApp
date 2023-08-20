bf95db63a30518ded294d0c747d810956fc0ab5d
Dennis Thinh Tan Nguyen <dennisnguyen3000@yahoo.dk>
2023-08-20 09:17:43 +0200

    add missing user endpoints

--- csharp/Web/Controllers/UsersController.cs
+++ csharp/Web/Controllers/UsersController.cs
@@ -10,15 +10,18 @@ public class UsersController : Controller
     private readonly ILogger<UsersController> _logger;
     private readonly IHandler<CreateUserRequest, CreateUserResponse> _createUserHandler;
     private readonly IHandler<GetUsersRequest, GetUsersResponse> _getUsersHandler;
+    private readonly IHandler<DeleteUserRequest, DeleteUserResponse> _deleteUserHandler;
 
     public UsersController(
         ILogger<UsersController> logger,
         IHandler<CreateUserRequest, CreateUserResponse> createUserHandler,
-        IHandler<GetUsersRequest, GetUsersResponse> getUsersHandler)
+        IHandler<GetUsersRequest, GetUsersResponse> getUsersHandler,
+        IHandler<DeleteUserRequest, DeleteUserResponse> deleteUserHandler)
     {
         _logger = logger;
         _createUserHandler = createUserHandler;
         _getUsersHandler = getUsersHandler;
+        _deleteUserHandler = deleteUserHandler;
     }
 
     [HttpPost]
@@ -32,6 +35,13 @@ public class UsersController : Controller
     public async Task<IActionResult> GetUsers([FromQuery] GetUsersRequest request)
     {
         GetUsersResponse response = await _getUsersHandler.Handle(request);
+        return Ok(response);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteUser([FromBody] DeleteUserRequest request)
+    {
+        DeleteUserResponse response = await _deleteUserHandler.Handle(request);
         return Ok(response);
     }
 }
